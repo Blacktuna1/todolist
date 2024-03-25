8f53a829bc2393ebc8b7f5a7b23c7192aa718034
Blacktuna1 <93248801+Blacktuna1@users.noreply.github.com>
2024-03-25 15:41:24 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>
+      TEST
+    </h1>
+    <todoList :todolist="todolist"></todoList>
+
+  </div>
+
+</template>
+
+<script>
+import todoList from './components/todoList.vue'
+export default {
+  name: "App",
+  data(){
+    return {
+      todolist:[
+        {id:1,task:'吃饭',isCompleted:true},
+        {id:1,task:'睡觉',isCompleted:false},
+        {id:1,task:'打豆豆',isCompleted:true},
+      ],
+    };
+  },
+  components: {
+    todoList
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/todoList.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul class="list-group">
+    <li 
+      class="list-group-item d-flex justify-content-between align-items-center"
+      v-for="item in todolist" :key="item.id"
+    >
+      <div class="form-group form-check">
+        <input 
+          type="checkbox" 
+          class="form-check-input" 
+          value=""
+          :id="item.id"
+          v-model="item.isCompleted"
+        >
+        <label class="form-check-label" :for="item.id">
+          {{item.task}}
+        </label>
+      </div>
+      <span class="badge badge-primary badge-pill" v-if="item.isCompleted">已完成</span>
+      <span class="badge badge-warning badge-pill" v-else>未完成</span>
+    </li>
+
+
+
+  </ul>
+</template>
+
+<script>
+export default {
+  name:'todoList',
+  props:{
+    todolist:{ 
+      type:Array,
+      required:true,
+      default:[]
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
